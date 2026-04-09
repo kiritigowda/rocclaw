b1d72a005d6094a6199a311d87e9c4c2ef2e5816
kiritigowda <kiritigowda@gmail.com>
2026-04-09 16:46:02 -0700

    Cleanup

--- src/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-// MIT License - Copyright (c) 2026 SimonCatBot
-// See LICENSE file for details.
-
-import { registerOTel } from "@vercel/otel";
-
-export const register = () => {
-  registerOTel({ serviceName: "openclaw-rocclaw" });
-};
